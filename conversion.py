6fbfb0c188f9d0916933b05834f561c9fa7ba42b
Carmine Laudato <56676908+carminelau@users.noreply.github.com>
2023-04-21 15:52:41 +0200

    first commit

---
+++ conversion.py
@@ -0,0 +1,15 @@
+import hashlib
+from concurrent.futures import ThreadPoolExecutor
+
+def hash_password(password):
+    with open("10MpassSHA1.txt", "a") as fw:
+        fw.write(hashlib.sha1(password.encode()).hexdigest() + ":" + password + "\n")
+
+with open("10Mpass.txt", "r") as fw:
+    with ThreadPoolExecutor() as executor:
+        for line in fw:
+            line = line.strip()
+            executor.submit(hash_password, line)
+            
+        
+        
